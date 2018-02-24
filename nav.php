71a74db6c9da18e68bef9aecb76708f6415d1f8a
Vikanda Gonzales <vikanda@Wynne.local>
2018-02-23 20:20:33 -0800

    installed bulma-start and dependencies; set up basic html, js, sass, php files; added brand work

---
+++ nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <img src="assets/brand/logo-wordmark.svg" alt="Vikanda Gonzales">
+    </a>
+    <div class="navbar-burger" data-target="navMenu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div class="navbar-menu" id="navMenu">
+    <a class="navbar-item">
+      About
+    </a>
+    <a class="navbar-item">
+      Skills
+    </a>
+    <a class="navbar-item">
+      Portfolio
+    </a>
+    <a class="navbar-item">
+      Blog
+    </a>
+    <a class="navbar-item">
+      Contact
+    </a>
+  </div>
+</nav>
