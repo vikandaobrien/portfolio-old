51a37e6f851490ff7b4ca615f7add2cff8ca67a1
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-02-25 11:50:22 -0800

    added respective links to social channels

--- social.php
+++ social.php
@@ -1,34 +1,34 @@
 <div class="field is-grouped">
   <p class="control">
-    <a class="button facebook">
+    <a class="button facebook" href="https://www.facebook.com/vikanda" target="_blank">
       <span class="icon">
         <i class="fab fa-facebook-f" aria-hidden="true"></i>
       </span>
     </a>
   </p>
   <p class="control">
-    <a class="button twitter">
+    <a class="button twitter" href="https://twitter.com/vikandagonzales" target="_blank">
       <span class="icon">
         <i class="fab fa-twitter" aria-hidden="true"></i>
       </span>
     </a>
   </p>
   <p class="control">
-    <a class="button linkedin">
+    <a class="button linkedin" href="https://www.linkedin.com/in/vikanda" target="_blank">
       <span class="icon">
         <i class="fab fa-linkedin-in" aria-hidden="true"></i>
       </span>
     </a>
   </p>
   <p class="control">
-    <a class="button github">
+    <a class="button github" href="https://github.com/vikandagonzales" target="_blank">
       <span class="icon">
         <i class="fab fa-github" aria-hidden="true"></i>
       </span>
     </a>
   </p>
   <p class="control">
-    <a class="button deviantart">
+    <a class="button deviantart" href="https://vikanda.deviantart.com" target="_blank">
       <span class="icon">
         <i class="fab fa-deviantart" aria-hidden="true"></i>
       </span>
