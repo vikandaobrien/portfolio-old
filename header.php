4d6ed40ff9a096c9bc1191c2690c3c60604ae544
Vikanda Gonzales <vikanda@Wynne.local>
2018-02-24 18:03:36 -0800

    added favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Vikanda Gonzales</title>
+    <link rel="icon" type="image/png" href="favicon.png">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/linearicons.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
